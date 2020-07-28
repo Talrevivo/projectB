bf1917e34b32b78605f569e9b08fbadc9addda0a
Talrevivo <37411352+Talrevivo@users.noreply.github.com>
2020-07-28 19:13:40 +0300

    Add files via upload

---
+++ BoundProject/jScripts/JavaScript.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("#menu-img").click(function () {
+        $(".nav-design").show();
+        $("#x").show();
+        $(".nav-design").addClass("navclick-design");
+    });
+
+    $("#x").click(function () {
+        $(".nav-design").hide();
+        $(".nav-design").addClass("navclick-design");
+        location.reload();
+    });
+});
+
+function openFullscreen() {
+    var elem = document.getElementById("PresentationForClass");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
